75b90c88531cc0fc1cd1f404ca089369ef7f88c2
Ron Rith <ronrith@Rons-Mac.local>
2018-05-21 12:57:19 +0700

    add tabbar and navigation

---
+++ notedandreferences.swift
@@ -0,0 +1,11 @@
+//
+//  notedandreferences.swift
+//  tabartest
+//
+//  Created by Ron Rith on 5/21/18.
+//  Copyright © 2018 Ron Rith. All rights reserved.
+//
+
+import Foundation
+
+//https://www.youtube.com/watch?v=qv-MOlsV3dw
